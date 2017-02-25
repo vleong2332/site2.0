26df1d79a8581231ba6acc40843632299cb3d911
Vicky Leong <vleong2332@gmail.com>
2017-02-25 17:45:51 -0500

    Make bubbles from SVG

--- assets/main.js
+++ assets/main.js
@@ -1,83 +1,50 @@
 document.addEventListener('DOMContentLoaded', function() {
-  makeBubbles();
+  makeSVGBubbles();
 });
 
 
-function makeBubbles(selector) {
+function makeSVGBubbles(selector) {
   selector = selector || '.bubble';
 
   var els = document.querySelectorAll(selector);
+  var xmlns = 'http://www.w3.org/2000/svg';
 
   for (var i = 0, length = els.length; i < length; i++) {
     var el = els[i];
-    var pos = getAbsPos(el);
-    var bubble = getConfig(el.dataset.bubble);
-    var ring = getConfig(el.dataset.ring);
-    var center = getConfig(el.dataset.center);
+    var strokeWidths = getStrokeWidths(el.dataset.strokeWidths);
 
-    el.style.width = bubble.width;
-    el.style.height = bubble.width;
-    el.style.position = 'absolute';
-    el.style['background-color'] = bubble.color;
-    el.style['border-radius'] = '100%';
-    for (var p in pos) {
-      el.style[p] = pos[p];
-    }
+    var svg = document.createElementNS(xmlns, 'svg');
+    svg.setAttributeNS(null, 'viewbox', '0 0 100 100');
+    svg.setAttributeNS(null, 'width', '100%');
+    svg.setAttributeNS(null, 'height', '100%');
+    el.appendChild(svg);
 
-    el.appendChild(createCircle(bubble.width, ring));
-    el.appendChild(createCircle(bubble.width, center));
+    var radius = svg.clientWidth * 0.5;
+
+    var c1 = document.createElementNS(xmlns, 'circle');
+    c1.setAttributeNS(null, 'r', radius - (strokeWidths.outter * 0.5));
+    c1.setAttributeNS(null, 'cx', radius);
+    c1.setAttributeNS(null, 'cy', radius);
+    c1.setAttributeNS(null, 'fill', 'transparent');
+    c1.setAttributeNS(null, 'stroke', 'rgba(0,0,0,0.15)');
+    c1.setAttributeNS(null, 'stroke-width', strokeWidths.outter);
+    svg.appendChild(c1);
+
+    var c2 = document.createElementNS(xmlns, 'circle');
+    c2.setAttributeNS(null, 'r', radius - (strokeWidths.inner * 0.5) - strokeWidths.outter);
+    c2.setAttributeNS(null, 'cx', radius);
+    c2.setAttributeNS(null, 'cy', radius);
+    c2.setAttributeNS(null, 'fill', 'rgba(255,255,255,0.1)');
+    c2.setAttributeNS(null, 'stroke', 'rgba(150,200,150,0.5)');
+    c2.setAttributeNS(null, 'stroke-width', strokeWidths.inner);
+    svg.appendChild(c2);
   }
-};
-
-
-function getAbsPos(el) {
-  var values = [];
-  var pos = {};
-  if (el.dataset.topLeft) {
-    values = el.dataset.topLeft.split(' ');
-    pos.top = values[0];
-    pos.left = values[1];
-  } else if (el.dataset.topRight) {
-    values = el.dataset.topRight.split(' ');
-    pos.top = values[0];
-    pos.right = values[1];
-  } else if (el.dataset.bottomRight) {
-    values = el.dataset.bottomRight.split(' ');
-    pos.bottom = values[0];
-    pos.right = values[1];
-  } else if (el.dataset.bottomLeft) {
-    values = el.dataset.bottomLeft.split(' ');
-    pos.bottom = values[0];
-    pos.left = values[1];
-  }
-  return pos;
 }
 
-function getConfig(el) {
-  if (!el) {
-    console.error("el ''", el, "' cannot be parsed into config");
-    return;
+function getStrokeWidths(values) {
+  var strokeWidths = values.split(' ');
+  return {
+    outter: strokeWidths[0] || undefined,
+    inner: strokeWidths[1] || undefined
   }
-
-  var config = {};
-  var values = el.split(' ');
-
-  config.width = values[0];
-  // TODO: check for valid value (px or %)
-  config.color = values[1] || 'inherit';
-  // TODO: check for valid value (short hex, long hex, or words)
-
-  return config;
-}
-
-function createCircle(bubbleWidth, config) {
-  var el = document.createElement('div');
-  el.style.width = config.width;
-  el.style.height = config.width;
-  el.style.position = 'absolute';
-  el.style['background-color'] = config.color;
-  el.style['border-radius'] = '100%';
-  el.style.top = 'calc(' + bubbleWidth + ' / 2 - ' + config.width + ' / 2)';
-  el.style.left = 'calc(' + bubbleWidth + ' / 2 - ' + config.width + ' / 2)';
-  return el;
 }
