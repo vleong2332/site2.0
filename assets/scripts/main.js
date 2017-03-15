7dbdde4a4d6151f0235ac0850171250eef40270b
Vicky Leong <vleong2332@gmail.com>
2017-03-15 09:20:14 -0400

    Add simple experience section

--- assets/scripts/main.js
+++ assets/scripts/main.js
@@ -1,18 +1,56 @@
 document.addEventListener('DOMContentLoaded', function() {
-  // Add drop shadow to the site-header when scroll position is not on top of
-  // the page
-  window.addEventListener('scroll', function() {
-    var body = document.querySelector('body');
-    var siteHeader = document.querySelector('.site-header');
-
-    if (body.scrollTop > 0 && !siteHeader.classList.contains('shadow')) {
-      siteHeader.classList.add('shadow');
-    } else if (body.scrollTop <= 0 && siteHeader.classList.contains('shadow')) {
-      siteHeader.classList.remove('shadow');
-    }
-
-  });
+  window.addEventListener('scroll', toggleSiteHeaderDropShadow);
+  window.addEventListener('resize', scrollToCurrentJob);
 });
+
+
+// Add drop shadow to the site-header when scroll position is not on top of
+// the page
+function toggleSiteHeaderDropShadow() {
+  var body = document.querySelector('body');
+  var siteHeader = document.querySelector('.site-header');
+
+  if (body.scrollTop > 0 && !siteHeader.classList.contains('shadow')) {
+    siteHeader.classList.add('shadow');
+  } else if (body.scrollTop <= 0 && siteHeader.classList.contains('shadow')) {
+    siteHeader.classList.remove('shadow');
+  }
+}
+
+// Move the timeline to center on the current job index
+function scrollToCurrentJob() {
+  var timeline = document.querySelector('.experience .timeline');
+  if (!timeline) { return; }
+  var currentIndex = timeline.dataset.currentIndex;
+  var timelineNode = document.querySelector('.experience .timeline-node[data-index="' + currentIndex + '"]');
+  timelineNode && timelineNode.onclick();
+}
+
+
+function scrollToJob(el, jobIndex) {
+  var timeline = el.parentElement;
+  var elWidth = el.getBoundingClientRect().width;
+  if (!timeline || !elWidth) { return; }
+  var marginWidth = parseFloat(window.getComputedStyle(el, '::after').getPropertyValue('width').replace('px', ''));
+  var left = (timeline.clientWidth * -0.5) + (elWidth * 0.5) + (elWidth * jobIndex) + (marginWidth * jobIndex);
+  timeline.style.left = left.toString() + 'px';
+  timeline.dataset.currentIndex = jobIndex.toString();
+
+  displayJob(jobIndex);
+}
+
+
+function displayJob(jobIndex) {
+  var jobs = document.querySelectorAll('.job');
+  for (var i = 0, length = jobs.length; i < length; i++) {
+    var currentEl = jobs[i];
+    if (currentEl.dataset.index === jobIndex.toString()) {
+      currentEl.classList.remove('hidden');
+    } else {
+      currentEl.classList.add('hidden');
+    }
+  }
+}
 
 
 function disableScrollOnBody(checkbox) {
